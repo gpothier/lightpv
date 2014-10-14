edcbc28d27206311368cb5aba3c36ad709e2ecde
gpothier <gpothier@gmail.com>
2014-10-14 00:16:14 -0300

    Cosmetic change

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 
 APPDIR=/opt/lightpv
 
-REMOTE_HEAD=`git ls-remote https://github.com/gpothier/lightpv.git HEAD|head -n 1|awk '{print $1}'`
-
 cd $APPDIR/src
+
+REMOTE_HEAD=`git ls-remote https://github.com/gpothier/lightpv.git HEAD|head -n 1|awk '{print $1}'`
 CURRENT_HEAD=`git log -n 1|head -n 1|awk '{print $2}'`
 
 echo "Remote:  $REMOTE_HEAD"
