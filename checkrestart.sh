2fb21acd9a07f42f635e16b19d4abc045de23dd5
gpothier <gpothier@gmail.com>
2014-10-14 00:27:04 -0300

    Formatting

--- checkrestart.sh
+++ checkrestart.sh
@@ -4,7 +4,7 @@ START_DATE=`cat /opt/lightpv/startdate`
 BUILD_DATE=`cat /opt/lightpv/builddate`
 
 echo "Start: $START_DATE"
-echo "Build:   $BUILD_DATE"
+echo "Build: $BUILD_DATE"
 
 if [ "0$BUILD_DATE" -gt "0$START_DATE" ]
 then
