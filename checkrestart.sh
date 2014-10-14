9087b9f5a7e0277b0b808e2b7fe7d49df5880196
gpothier <gpothier@gmail.com>
2014-10-14 00:23:37 -0300

    Handle first execution

--- checkrestart.sh
+++ checkrestart.sh
@@ -6,7 +6,7 @@ BUILD_DATE=`cat /opt/lightpv/builddate`
 echo "Start: $START_DATE"
 echo "Build:   $BUILD_DATE"
 
-if [ "$BUILD_DATE" -gt "$START_DATE" ]
+if [ "0$BUILD_DATE" -gt "0$START_DATE" ]
 then
 	echo "Restarting"
 	echo service lightpv restart
